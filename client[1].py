b3a5d5b65d0100497096770d19d7895047f3ba5e
Umer Farooq <k142229@nu.edu.pk>
2017-03-19 05:37:10 -0800

    Add files via upload

---
+++ client[1].py
@@ -0,0 +1,13 @@
+import os
+from socket import *
+host = "192.168.109.131 " 
+port = 13001
+addr = (host, port)
+UDPSock = socket(AF_INET, SOCK_DGRAM)
+while True:
+    data = raw_input("Enter message to send or type 'exit': ")
+    UDPSock.sendto(data, addr)
+    if data == "exit":
+        break
+UDPSock.close()
+os._exit(0)
